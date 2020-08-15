42b3bdf67ebbc5818c5d5dad06ddf6f1de9682d4
Rysiek <ryspol@onet.pl>
2020-08-15 12:39:59 +0200

    http+s change

--- components/api.js
+++ components/api.js
@@ -6,13 +6,13 @@ const getAPI = async (city, type = "current") => // city to miasto pobrane z inp
     switch (type)
     {
         case "current": //bierząca pogoda
-            apiAdress = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pl&units=metric&APPID=37bc5c2efba7f70f2b60dd00061732e8`;
+            apiAdress = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pl&units=metric&APPID=37bc5c2efba7f70f2b60dd00061732e8`;
             break;
         // case "3 days": //pogoda na 3 dni
         //     apiAdress = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&lang=pl&cnt=24&APPID=436d1eea572e5d346f8cd5eb7c8dfa14`;
         //     break;
         case "5 days": //pogoda na 5 dni
-            apiAdress = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&lang=pl&APPID=37bc5c2efba7f70f2b60dd00061732e8`;
+            apiAdress = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&lang=pl&APPID=37bc5c2efba7f70f2b60dd00061732e8`;
             break;
         default:
             throw "Wrong type."
